d23d7b154834f2dc229a8e58283cdf94599605aa
Hristiyan06 <138652718+Hristiyan06@users.noreply.github.com>
2023-07-07 13:18:13 +0300

    WinCondition

--- Tic-Tac-Toe/Pages/Index.cshtml.cs
+++ Tic-Tac-Toe/Pages/Index.cshtml.cs
@@ -10,6 +10,7 @@ namespace Tic_Tac_Toe.Pages
         private readonly ILogger<IndexModel> _logger;
         [BindProperty]
         public int Cell { get; set; }
+        public string Win { get; set; }
 
         private Dictionary<int, string> selectedCells;
 
@@ -56,7 +57,12 @@ namespace Tic_Tac_Toe.Pages
             SelectedCells = TempData.Get<Dictionary<int, string>>("Selected");
 
             SelectedCells[Cell] = "X";
+            WinCondition();
 
+            if (Win != null)
+            {
+                return;
+            }
             Random rnd = new Random();
             int num = rnd.Next(1, 10);
 
@@ -74,10 +80,88 @@ namespace Tic_Tac_Toe.Pages
 
                 SelectedCells[num] = "O";
             }
+            if (Win != null)
+            {
+                return;
+            }
 
             TempData.Set("Selected", SelectedCells);
 
             TempData.Keep("SelectedCells");
+
+
+        }
+        public void WinCondition()
+        {
+            if (SelectedCells[1] == "X" && SelectedCells[2] == "X" && SelectedCells[3] == "X")
+            {
+                Win = "You win";
+            }
+            if (selectedCells[4] == "X" && SelectedCells[5] == "X" && SelectedCells[6] == "X")
+            {
+                Win = "You win";
+            }
+            if (SelectedCells[7] == "X" && SelectedCells[8] == "X" && SelectedCells[9] == "X")
+            {
+                Win = "You win";
+            }
+            if (SelectedCells[1] == "X" && SelectedCells[4] == "X" && SelectedCells[7] == "X")
+            {
+                Win = "You win";
+            }
+            if (SelectedCells[2] == "X" && SelectedCells[5] == "X" && SelectedCells[8] == "X")
+            {
+                Win = "You win";
+            }
+            if (SelectedCells[3] == "X" && SelectedCells[6] == "X" && SelectedCells[9] == "X")
+            {
+                Win = "You win";
+            }
+            if (SelectedCells[1] == "X" && SelectedCells[5] == "X" && SelectedCells[9] == "X")
+            {
+                Win = "You win";
+            }
+            if (SelectedCells[3] == "X" && SelectedCells[5] == "X" && SelectedCells[7] == "X")
+            {
+                Win = "You win";
+            }
+
+
+
+
+
+            if (SelectedCells[1] == "O" && SelectedCells[2] == "O" && SelectedCells[3] == "O")
+            {
+                Win = "You lose";
+            }
+            if (selectedCells[4] == "O" && SelectedCells[5] == "O" && SelectedCells[6] == "O")
+            {
+                Win = "You lose";
+            }
+            if (SelectedCells[7] == "O" && SelectedCells[8] == "O" && SelectedCells[9] == "O")
+            {
+                Win = "You lose";
+            }
+            if (SelectedCells[1] == "O" && SelectedCells[4] == "O" && SelectedCells[7] == "O")
+            {
+                Win = "You lose";
+            }
+            if (SelectedCells[2] == "O" && SelectedCells[5] == "O" && SelectedCells[8] == "O")
+            {
+                Win = "You lose";
+            }
+            if (SelectedCells[3] == "O" && SelectedCells[6] == "O" && SelectedCells[9] == "O")
+            {
+                Win = "You lose";
+            }
+            if (SelectedCells[1] == "O" && SelectedCells[5] == "O" && SelectedCells[9] == "O")
+            {
+                Win = "You lose";
+            }
+            if (SelectedCells[3] == "O" && SelectedCells[5] == "O" && SelectedCells[7] == "O")
+            {
+                Win = "You lose";
+            }
         }
     }
 }
