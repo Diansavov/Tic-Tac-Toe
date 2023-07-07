fa2b42f32a98d2993f9196b9c51c22f2d839af4f
diansavov <diansavov2@abv.bg>
2023-07-07 11:48:58 +0300

    TempData Save

--- Tic-Tac-Toe/Pages/Index.cshtml.cs
+++ Tic-Tac-Toe/Pages/Index.cshtml.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Tic_Tac_Toe.Helpers;
 
 namespace Tic_Tac_Toe.Pages
 {
@@ -9,23 +11,34 @@ namespace Tic_Tac_Toe.Pages
         [BindProperty]
         public int Cell { get; set; }
 
-        private Dictionary<int, string> selectedCells = new Dictionary<int, string>(9)
-        {
-            {1, "" },
-            {2, "" },
-            {3, "" },
-            {4, "" },
-            {5, "" },
-            {6, "" },
-            {7, "" },
-            {8, "" },
-            {9, "" },
-        };
+        private Dictionary<int, string> selectedCells;
 
         public Dictionary<int, string> SelectedCells
         {
-             get{ return selectedCells; } 
-             set{ selectedCells = value; } 
+            get
+            {
+                if (selectedCells == null)
+                {
+                    selectedCells = new Dictionary<int, string>(9)
+                    {
+                        {1, "" },
+                        {2, "" },
+                        {3, "" },
+                        {4, "" },
+                        {5, "" },
+                        {6, "" },
+                        {7, "" },
+                        {8, "" },
+                        {9, "" },
+                    };
+                }
+
+                return selectedCells;
+            }
+            set
+            {
+                selectedCells = value;
+            }
         }
 
         public IndexModel(ILogger<IndexModel> logger)
@@ -35,22 +48,34 @@ namespace Tic_Tac_Toe.Pages
 
         public void OnGet()
         {
-
+            selectedCells = null;
         }
 
         public void OnPost(int cell)
         {
+            SelectedCells = TempData.Get<Dictionary<int, string>>("Selected");
+
             SelectedCells[Cell] = "X";
 
             Random rnd = new Random();
             int num = rnd.Next(1, 10);
 
-            while (SelectedCells[num] == "X")
+            while (SelectedCells[num] == "X" || SelectedCells[num] == "O")
             {
                 num = rnd.Next(1, 10);
+
+                if (!SelectedCells.ContainsValue(""))
+                {
+                    break;
+                }
+            }
+            if (SelectedCells[num] != "X" && SelectedCells[num] != "O")
+            {
+
+                SelectedCells[num] = "O";
             }
 
-            SelectedCells[num] = "O";
+            TempData.Set("Selected", SelectedCells);
 
             TempData.Keep("SelectedCells");
         }
